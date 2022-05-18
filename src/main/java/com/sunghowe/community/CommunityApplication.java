a1a09475363448f9a870b7f0ce729476e8cde048
sunghowe <3466055918@qq.com>
2022-05-18 18:00:39 +0800

    首次提交

--- src/main/java/com/sunghowe/community/CommunityApplication.java
+++ src/main/java/com/sunghowe/community/CommunityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommunityApplication {
 
 	public static void main(String[] args) {
+		//测试改动
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 
