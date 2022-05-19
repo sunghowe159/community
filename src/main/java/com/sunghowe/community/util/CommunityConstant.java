505cb453e2d77698ff4f7d81aae4aa72705bffcc
sunghowe <3466055918@qq.com>
2022-05-19 16:26:00 +0800

    完成"开发注册功能"模块

---
+++ src/main/java/com/sunghowe/community/util/CommunityConstant.java
@@ -0,0 +1,24 @@
+package com.sunghowe.community.util;
+
+/**
+ * @author SungHowe
+ * @title CommunityConstant
+ * @description
+ * @create 2022-05-19 16:02
+ */
+public interface CommunityConstant {
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+}
---
+++ src/main/java/com/sunghowe/community/util/CommunityUtil.java
@@ -0,0 +1,27 @@
+package com.sunghowe.community.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+/**
+ * @author SungHowe
+ * @title CommunityUtil
+ * @description
+ * @create 2022-05-19 14:53
+ */
+public class CommunityUtil {
+    //生成随机字符串
+    public static String generateUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    //MD5加密
+    public static String md5(String key) {
+        if (StringUtils.isBlank(key)) {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+}
