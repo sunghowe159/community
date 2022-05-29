2929db6b045170cee1eb1e28cc4cb6cc368bb5b5
sunghowe <3466055918@qq.com>
2022-05-29 16:48:03 +0800

    完成"添加评论功能"模块

---
+++ src/main/java/com/sunghowe/community/controller/CommentController.java
@@ -0,0 +1,39 @@
+package com.sunghowe.community.controller;
+
+import com.sunghowe.community.entity.Comment;
+import com.sunghowe.community.service.CommentService;
+import com.sunghowe.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+/**
+ * @author SungHowe
+ * @title CommentController
+ * @description
+ * @create 2022-05-29 16:22
+ */
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+
+}
