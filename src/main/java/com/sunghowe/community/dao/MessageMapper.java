60e612fc8a66a161135f3dc84a9d4fc7d70c1eef
sunghowe <3466055918@qq.com>
2022-05-30 12:43:48 +0800

    完成"私信列表"模块

---
+++ src/main/java/com/sunghowe/community/dao/MessageMapper.java
@@ -0,0 +1,32 @@
+package com.sunghowe.community.dao;
+
+import com.sunghowe.community.entity.Message;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author SungHowe
+ * @title MessageMapper
+ * @description
+ * @create 2022-05-30 9:10
+ */
+@Mapper
+public interface MessageMapper {
+
+    // 查询当前用户的会话列表，针对某个会话只返回一条最新的私信
+    List<Message> selectConversations(int userId, int offset, int limit);
+
+    // 查询当前用户的会话数量
+    int selectConversationCount(int userId);
+
+    // 查询某个会话所包含的私信列表
+    List<Message> selectLetters(String conversationId, int offset, int limit);
+
+    // 查询某个会话所包含的私信数量
+    int selectLetterCount(String conversationId);
+
+    // 查询未读私信的数量
+    int selectLetterUnreadCount(int userId, String conversationId);
+
+}
---
+++ src/main/java/com/sunghowe/community/service/MessageService.java
@@ -0,0 +1,40 @@
+package com.sunghowe.community.service;
+
+import com.sunghowe.community.dao.MessageMapper;
+import com.sunghowe.community.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author SungHowe
+ * @title MessageService
+ * @description
+ * @create 2022-05-30 9:45
+ */
+@Service
+public class MessageService {
+    @Autowired
+    private MessageMapper messageMapper;
+
+    public List<Message> findConversations(int userId, int offset, int limit) {
+        return messageMapper.selectConversations(userId, offset, limit);
+    }
+
+    public int findConversationCount(int userId) {
+        return messageMapper.selectConversationCount(userId);
+    }
+
+    public List<Message> findLetters(String conversationId, int offset, int limit) {
+        return messageMapper.selectLetters(conversationId, offset, limit);
+    }
+
+    public int findLetterCount(String conversationId) {
+        return messageMapper.selectLetterCount(conversationId);
+    }
+
+    public int findLetterUnreadCount(int userId, String conversationId) {
+        return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+}
