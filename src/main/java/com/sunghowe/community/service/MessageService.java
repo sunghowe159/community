f1074a50b347709987c16ccbabc2eea52090c7c9
sunghowe <3466055918@qq.com>
2022-05-30 14:20:13 +0800

    完成"发送私信"模块

--- src/main/java/com/sunghowe/community/service/MessageService.java
+++ src/main/java/com/sunghowe/community/service/MessageService.java
@@ -2,8 +2,10 @@ package com.sunghowe.community.service;
 
 import com.sunghowe.community.dao.MessageMapper;
 import com.sunghowe.community.entity.Message;
+import com.sunghowe.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -17,6 +19,8 @@ import java.util.List;
 public class MessageService {
     @Autowired
     private MessageMapper messageMapper;
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<Message> findConversations(int userId, int offset, int limit) {
         return messageMapper.selectConversations(userId, offset, limit);
@@ -36,5 +40,15 @@ public class MessageService {
 
     public int findLetterUnreadCount(int userId, String conversationId) {
         return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+
+    public int addMessage(Message message) {
+        message.setContent(HtmlUtils.htmlEscape(message.getContent()));
+        message.setContent(sensitiveFilter.filter(message.getContent()));
+        return messageMapper.insertMessage(message);
+    }
+
+    public int readMessage(List<Integer> ids) {
+        return messageMapper.updateStatus(ids, 1);
     }
 }
