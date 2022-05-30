2f0d8e1508bc665443e86f8aa39810abe973020d
sunghowe <3466055918@qq.com>
2022-05-30 14:56:28 +0800

    完成"删除私信"模块

--- src/main/java/com/sunghowe/community/controller/MessageController.java
+++ src/main/java/com/sunghowe/community/controller/MessageController.java
@@ -7,6 +7,8 @@ import com.sunghowe.community.service.MessageService;
 import com.sunghowe.community.service.UserService;
 import com.sunghowe.community.util.CommunityUtil;
 import com.sunghowe.community.util.HostHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +27,7 @@ import java.util.*;
  */
 @Controller
 public class MessageController {
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 
     @Autowired
     private MessageService messageService;
@@ -135,6 +138,13 @@ public class MessageController {
         message.setContent(content);
         message.setCreateTime(new Date());
         messageService.addMessage(message);
+        return CommunityUtil.getJSONString(0);
+    }
+
+    @RequestMapping(path = "/letter/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String deleteLetter(int id) {
+        messageService.deleteMessage(id);
         return CommunityUtil.getJSONString(0);
     }
 }
