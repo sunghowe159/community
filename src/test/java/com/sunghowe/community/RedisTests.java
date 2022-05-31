e76b630300502baac13e781cdb725afd90de2e95
sunghowe <3466055918@qq.com>
2022-05-31 10:46:43 +0800

    完成"整合Redis"模块

---
+++ src/test/java/com/sunghowe/community/RedisTests.java
@@ -0,0 +1,39 @@
+package com.sunghowe.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author SungHowe
+ * @title RedisTests
+ * @description
+ * @create 2022-05-31 10:20
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings(){
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHash(){
+        String redisKey = "test:hash";
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "TOM");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+}
