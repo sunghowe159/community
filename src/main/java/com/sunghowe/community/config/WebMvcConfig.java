907b26b54c74ee7947d294fb40f21f2194c65b01
sunghowe <3466055918@qq.com>
2022-06-06 15:50:54 +0800

    完成"显示系统通知"模块

--- src/main/java/com/sunghowe/community/config/WebMvcConfig.java
+++ src/main/java/com/sunghowe/community/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.sunghowe.community.config;
 import com.sunghowe.community.controller.interceptor.AlphaInterceptor;
 import com.sunghowe.community.controller.interceptor.LoginRequiredInterceptor;
 import com.sunghowe.community.controller.interceptor.LoginTicketInterceptor;
+import com.sunghowe.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -23,6 +24,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private LoginTicketInterceptor loginTicketInterceptor;
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
+    @Autowired
+    private MessageInterceptor messageInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -34,6 +37,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
