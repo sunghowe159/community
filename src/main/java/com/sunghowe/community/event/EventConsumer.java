c42312504da5c85b84032bdef0bd22bcddeeaf5b
sunghowe <3466055918@qq.com>
2022-06-16 20:58:38 +0800

    完成“置顶、加精、删除”模块

--- src/main/java/com/sunghowe/community/event/EventConsumer.java
+++ src/main/java/com/sunghowe/community/event/EventConsumer.java
@@ -38,7 +38,7 @@ public class EventConsumer implements CommunityConstant {
 
     @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
     public void handleCommentMessage(ConsumerRecord record) {
-        if (record == null || record.value() ==null) {
+        if (record == null || record.value() == null) {
             logger.error("消息的内容为空！");
             return;
         }
@@ -72,7 +72,7 @@ public class EventConsumer implements CommunityConstant {
     // 消费发帖事件
     @KafkaListener(topics = {TOPIC_PUBLISH})
     public void handlePublishMessage(ConsumerRecord record) {
-        if (record == null || record.value() ==null) {
+        if (record == null || record.value() == null) {
             logger.error("消息的内容为空！");
             return;
         }
@@ -84,5 +84,21 @@ public class EventConsumer implements CommunityConstant {
 
         DiscussPost post = discussPostService.findDiscussPostById(event.getEntityId());
         elasticsearchService.saveDiscussPost(post);
+    }
+
+    // 消费删帖事件
+    @KafkaListener(topics = {TOPIC_DELETE})
+    public void handleDeleteMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息的内容为空！");
+            return;
+        }
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误！");
+            return;
+        }
+
+        elasticsearchService.deleteDiscussPost(event.getEntityId());
     }
 }
