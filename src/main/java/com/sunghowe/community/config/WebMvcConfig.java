d5e16f518e04164956a4fede79f7d0535d5c9147
sunghowe <3466055918@qq.com>
2022-06-17 22:36:25 +0800

    完成“网站数据统计”模块

--- src/main/java/com/sunghowe/community/config/WebMvcConfig.java
+++ src/main/java/com/sunghowe/community/config/WebMvcConfig.java
@@ -1,9 +1,6 @@
 package com.sunghowe.community.config;
 
-import com.sunghowe.community.controller.interceptor.AlphaInterceptor;
-import com.sunghowe.community.controller.interceptor.LoginRequiredInterceptor;
-import com.sunghowe.community.controller.interceptor.LoginTicketInterceptor;
-import com.sunghowe.community.controller.interceptor.MessageInterceptor;
+import com.sunghowe.community.controller.interceptor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -26,6 +23,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //    private LoginRequiredInterceptor loginRequiredInterceptor;
     @Autowired
     private MessageInterceptor messageInterceptor;
+    @Autowired
+    private DataInterceptor dataInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -40,6 +39,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(dataInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
---
+++ src/main/java/com/sunghowe/community/controller/interceptor/DataInterceptor.java
@@ -0,0 +1,42 @@
+package com.sunghowe.community.controller.interceptor;
+
+import com.sunghowe.community.entity.User;
+import com.sunghowe.community.service.DataService;
+import com.sunghowe.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author SungHowe
+ * @title DataInterceptor
+ * @description
+ * @create 2022-06-17 20:28
+ */
+@Component
+public class DataInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private DataService dataService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 统计UV
+        String ip = request.getRemoteHost();
+        dataService.recordUV(ip);
+
+        // 统计DAU
+        User user = hostHolder.getUser();
+        if (user != null) {
+            dataService.recordDAU(user.getId());
+        }
+
+        return true;
+    }
+}
